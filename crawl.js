5b2ffc112383c943af6354b76ee991a2f32f2630
yigit ocak <yigitockk@gmail.com>
2024-07-10 20:03:29 -0400

    normalizeURL and tests

---
+++ crawl.js
@@ -0,0 +1,8 @@
+export const normalizeURL = (urlString) => {
+  const urlObj = new URL(urlString);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
+};
